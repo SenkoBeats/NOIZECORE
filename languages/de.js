5ecf77827fce9464aa78702b8eac0c872ff2d0b8
SenkoBeats <zest.music.production@gmail.com>
2025-08-07 22:05:19 +0200

    de.js

--- languages/de.js
+++ languages/de.js
@@ -8,7 +8,7 @@ module.exports = {
         responseTime: "- Bot-Antwortzeit: **{latency}ms**",
         websocketPing: "- WebSocket-Ping: **{ping}ms**",
         uptime: "- Betriebszeit: **{uptime}**",
-        footer: "Entwickelt von SSRR | Prime Music v1.2"
+        footer: "Entwickelt von SenkoBeats | NOIZECORE"
       }
     },
     addsong: {
@@ -277,3 +277,4 @@ module.exports = {
     }
 
 };
+
